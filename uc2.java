4ca2f238e7b2daabe9c84ba593374604b995ee19
GAURANG SHARMA <gs1519@srmist.edu.in>
2026-03-03 08:13:21 +0530

    Add files via upload

---
+++ uc2.java
@@ -0,0 +1,12 @@
+public class uc2 {
+    public static void main(String[] args) {
+        // Print each line of the OOPS banner to create the visual effect
+        System.out.println("  ***         ***       ******     ***** ");
+        System.out.println(" **  **      **  **     **   **   **   **");
+        System.out.println("**    **    **    **    **   **   **     ");
+        System.out.println("**    **    **    **    ******     ***   ");
+        System.out.println("**    **    **    **    **           **  ");
+        System.out.println(" **  **      **  **     **            ** ");
+        System.out.println("  ***         ***       **        *****  ");
+    }   
+}
